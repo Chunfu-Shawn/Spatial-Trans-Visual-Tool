d7f26a9ef5e88b488fee4511e38f41747362d316
Simons-Maddy <xiaochunfu@126.com>
2022-09-06 22:39:18 +0800

    update readme

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b4af3d837c56c5d9"; }
+/******/ 		__webpack_require__.h = function() { return "b72dceca171945cf"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
